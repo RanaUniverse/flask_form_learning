346683c823bea3d78ceb7897bc2ef9550fdbc26a
RanaUniverse <RanaUniverse321@gmail.com>
2025-09-28 05:58:18 +0530

    now user can directly register a new account in /register and then /login  there

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,18 @@
+from collections.abc import Sequence
+from typing import Mapping
 from flask_wtf import FlaskForm  # type: ignore # for this has what to do i am confused.
+
+import sqlalchemy as sa
 
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from wtforms.validators import DataRequired
+
+
+from app import db
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -29,3 +39,37 @@ class LoginForm(FlaskForm):
     submit = SubmitField(
         "Sign In",
     )
+
+
+class RegistrationForm(FlaskForm):
+
+    username = StringField(
+        label="Username",
+        validators=[DataRequired()],
+    )
+
+    email = StringField(
+        label="Email",
+        validators=[DataRequired(), Email()],
+    )
+    password = PasswordField(
+        label="Password",
+        validators=[DataRequired()],
+    )
+
+    password2 = PasswordField(
+        label="Repeat Password",
+        validators=[DataRequired(), EqualTo("password")],
+    )
+
+    submit = SubmitField("Register Now")
+
+    def validate_username(self, username: StringField):
+        user = db.session.scalar(sa.select(User).where(User.username == username.data))
+        if user is not None:
+            raise ValidationError("Please use a different username.")
+
+    def validate_email(self, email: StringField):
+        user = db.session.scalar(sa.select(User).where(User.email == email.data))
+        if user is not None:
+            raise ValidationError("Please use a different email address.")
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,13 @@
-from flask import render_template, flash, redirect, url_for
+from urllib.parse import urlsplit
 
-from app import app
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
 
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+
+from app import app, db
+from app.models import User
+from app.forms import LoginForm, RegistrationForm
 
 user = {"username": "Roman Reigns"}
 
@@ -20,40 +25,88 @@ posts: list[dict[str, str | dict[str, str]]] = [
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
     return render_template(
         "index.html",
         title="Home",
-        user=user,
+        # user=user,
         posts=posts,
-    )
-
-
-@app.route("/login_old")
-def login_old():
-    form = LoginForm()
-    return render_template(
-        "login.html",
-        title="Sign In Page",
-        form=form,
     )
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
-    print("is this validate on submit?", form.validate_on_submit())
     if form.validate_on_submit():
-        flash(
-            "Login requested for user {}; remember_me={}".format(
-                form.username.data,
-                form.remember_me.data,
-            )
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)
         )
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid username or password was pressed...")
+            return redirect(url_for("login"))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get("next")
+        if not next_page or urlsplit(next_page).netloc != "":
+            next_page = url_for("index")
+        return redirect(next_page)
+    return render_template("login.html", title="Sign In", form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
         return redirect(url_for("index"))
 
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations, you are now register as a new user")
+        return redirect(url_for("login"))
     return render_template(
-        "login.html",
-        title="Sign In Page",
+        "register.html",
+        title="Register For New User",
         form=form,
     )
+
+
+# @app.route("/login_old")
+# def login_old():
+#     form = LoginForm()
+#     return render_template(
+#         "login.html",
+#         title="Sign In Page",
+#         form=form,
+#     )
+
+
+# @app.route("/login_old2", methods=["GET", "POST"])
+# def login_old2():
+#     form = LoginForm()
+#     print("is this validate on submit?", form.validate_on_submit())
+#     if form.validate_on_submit():
+#         flash(
+#             "Login requested for user {}; remember_me={}".format(
+#                 form.username.data,
+#                 form.remember_me.data,
+#             )
+#         )
+#         return redirect(url_for("index"))
+
+#     return render_template(
+#         "login.html",
+#         title="Sign In Page",
+#         form=form,
+#     )
